fbada2fe14b7cade4494c80a345f9d8ffd4be107
Juan Parera <1420988+jparera@users.noreply.github.com>
2024-02-04 10:18:43 +0100

    Enable number column on FileType event

--- lua/wrlt/set.lua
+++ lua/wrlt/set.lua
@@ -3,10 +3,6 @@ vim.opt.undofile = true
 vim.opt.timeoutlen = 3000
 vim.opt.ttimeoutlen = 100
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-
 vim.opt.scrolloff = 8
 
 vim.opt.list = true
@@ -28,8 +24,10 @@ vim.api.nvim_create_autocmd('FileType', {
     desc = 'Configures local options for programming languages.',
     group = wrlt,
     callback = function()
+        vim.opt_local.number = true
+        vim.opt_local.relativenumber = true
+        vim.opt_local.cursorline = true
         vim.opt_local.signcolumn = 'yes'
         vim.opt_local.colorcolumn = { '80', '100' }
     end
 })
-
